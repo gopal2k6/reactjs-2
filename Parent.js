2acbd9ffeeb1cca9932531af40c1684c2acfc52f
gopal2k6 <gopal2k6.1983@gmail.com>
2022-09-04 19:20:16 +0530

    2 files added, 2 deleted

---
+++ Parent.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+
+export default class Parent extends React.Components {
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import Parent from './Parent';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-    <App />
-);
+root.render(<Parent />);
